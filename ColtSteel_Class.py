d30c06123772bfb3d8ac67558d607cc12099a567
Miguel Perez <miguel.p.donate@gmail.com>
2023-09-07 14:09:27 +0200

    Progress

---
+++ ColtSteel_Class.py
@@ -0,0 +1,104 @@
+""" Bank Robbery Exercise
+I just robbed a bank with some of my friends. 
+We got away with $19,867,324,678,987.99, and now we have to split it up 5 ways. 
+Using the cash  variable I've already defined for you, print out the dollar amount each robber gets to keep. 
+(divide cash  by 5 and print the answer out)
+
+p.s. I haven't actually robbed a bank.  Yet."""
+
+cash = 19867324678987.99  # DON'T CHANGE THE CASH VARIABLE
+
+# ADD YOUR CODE BELOW:
+print(cash / 5)
+
+
+
+
+"""Make Some Variables!
+Now that we've learned about variables and data types, let's get some practice.  Or, skip this! Totally up to you!
+
+Define a variable named city  and set it equal to any string
+Define a variable named price  and set it equal to any float
+Define a variable named high_score  and set it equal to any int
+Define a variable named is_having_fun  and set it to a boolean value
+You do not need to print them out, but can if you want."""
+
+
+# Define a variable named city and set it equal to any string
+
+# Define a variable named price and set it equal to any float
+
+# Define a variable named high_score and set it equal to any int
+
+# Define a variable named is_having_fun and set it to a Boolean value
+city = "New York"
+price = 5.6
+high_score = 7
+is_having_fun = True
+
+
+
+
+
+"""Escape Sequence Practice
+Get some practice with escape sequences!  
+NO need to create new variables, 
+just use the existing variables we have created for you and follow the instructions. :) """
+
+
+ # Set the message variable equal to any string containing a new-line escape sequence
+message = "Hello \n goodbye"
+ 
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+ 
+mountains = "/\\/\\/\\"
+ 
+ 
+# Set the quotation variable to any string that contains an escaped double quotation mark
+quotation = "My cat said \"meow\""
+
+
+
+
+
+"""String Concatenation Exercise
+Set the variable called greeting  to some greeting, e.g. "hello".
+
+Set the variable called name  to some name, e.g. "Heisenberg".
+
+Then set the variable called greet_name  so that it concatenates greeting , name , and a space " " between them."""
+
+greeting = "Hi"
+name = "Miguel"
+greet_name = greeting + " " + name
+
+
+
+"""Formatting Strings
+Set the variable called first  to your first name.
+
+Set the variable called last  to your last name.
+
+Then set the variable called formatted  to a new string that interpolates both values (from the first and last variables) using f-strings or the .format()  method.  Make sure you follow this exact pattern for the new string that you store to the formatted variable (pay attention to capital/lowercase letters, spaces, commas, etc):
+
+"First Name: Colt, Last Name: Steele"""
+
+first = "Miguel"
+last = "Perez"
+
+formatted = f"First Name: {first}, Last Name: {last}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
